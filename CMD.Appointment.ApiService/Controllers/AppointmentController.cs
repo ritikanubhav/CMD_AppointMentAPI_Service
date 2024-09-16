5eb3aed98c78373b2bf83c045f03d6a9455370f3
Ritik Anubhav <Ritik.Anubhav@consilio.com>
2024-09-16 12:38:48 +0530

    push

--- CMD.Appointment.ApiService/Controllers/AppointmentController.cs
+++ CMD.Appointment.ApiService/Controllers/AppointmentController.cs
@@ -24,6 +24,7 @@ namespace CMD.Appointment.ApiService.Controllers
         /// </summary>
         /// <param name="appointmentManager">The appointment manager service.</param>
         /// <param name="messageService">The message service.</param>
+        /// 
         public AppointmentController(IAppointmentManager appointmentManager, IMessageService messageService)
         {
             this.appointmentManager = appointmentManager;
